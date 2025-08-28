44a0814add470ef3c50fc96d1b535935469b9417
soumili-03 <soumili.biswas3183@gmail.com>
2025-08-29 01:21:00 +0530

    Update app message to version 2.0

--- sample-app/app.js
+++ sample-app/app.js
@@ -2,9 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Jenkins CI/CD Pipeline!\\n');
+  res.end('Hello from Jenkins CI/CD Pipeline - Version 2.0!\n');
 });
 
 server.listen(3000, () => {
-  console.log('App running on port 3000');
+  console.log('App running on port 3000 - Version 2.0');
 });
